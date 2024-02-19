fcf8574b11c7c0ef46cec5ba23dcc18c3c174bc0
Emma Hubert <ehubert@email.sc.edu>
2024-02-19 16:36:00 -0500

    4.5 added

--- Projects/part3/creators/script.js
+++ Projects/part3/creators/script.js
@@ -2,4 +2,9 @@ const showHideNav = () => {
     document.getElementById("nav-items").classList.toggle("hide-small");
 };
 
+const logoLink = () => {
+    window.location.href= "https://ehubertt.github.io/Projects/part3/home/index.html";
+}
+
+document.getElementById("logo").onclick = logoLink;
 document.getElementById("hamburger").onclick = showHideNav;
--- Projects/part3/favorites/script.js
+++ Projects/part3/favorites/script.js
@@ -2,6 +2,11 @@ const showHideNav = () => {
     document.getElementById("nav-items").classList.toggle("hide-small");
 };
 
+const logoLink = () => {
+  window.location.href= "https://ehubertt.github.io/Projects/part3/home/index.html";
+}
+
+document.getElementById("logo").onclick = logoLink;
 document.getElementById("hamburger").onclick = showHideNav;
 
 /* When the user clicks on the button,
