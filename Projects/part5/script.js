811158d6095a9f4f2f98690ffa8d7eeccf835242
Emma Hubert <ehubert@email.sc.edu>
2024-03-12 14:52:11 -0400

    update

---
+++ Projects/part5/script.js
@@ -0,0 +1,64 @@
+const showHideNav = () => {
+    document.getElementById("nav-items").classList.toggle("hide-small");
+};
+const logoLink = () => {
+    window.location.href= "https://ehubertt.github.io/Projects/part3/home/index.html";
+}
+
+document.getElementById("logo").onclick = logoLink;
+document.getElementById("hamburger").onclick = showHideNav;
+
+const getBrands  = async () => {
+    const url = "https://ehubertt.github.io/Projects/part5/brands.json";
+
+    try{
+        const response = await fetch(url);
+        return response.json();
+    }catch(error){
+        console.log(error);
+    }
+}
+
+const showBrands = async() => {
+    const brands = await getBrands();
+    const brandList = document.getElementById("main-content");
+
+    brands.forEach(brand => {
+        brandList.append(getBrandSection(brand));
+    })
+}
+
+const getBrandSection = (brand) => {
+    //create div for two brands to flex 
+    const div = document.createElement("section");
+    div.className = "brand-box";
+    //create section for brand 
+    const brandSection = document.createElement("section");
+    brandSection.className = "brand";
+    div.appendChild(brandSection);
+
+    // create section for name inside brand section
+    const name = document.createElement("section");
+    name.className = "name";
+    name.innerHTML = `${brand.name}`
+    brandSection.appendChild(name);
+
+    //add image section below name 
+    const image = document.createElement("img");
+    image.className = "images";
+    image.src = `https:ehubert.github.io/projects/part5/images/${brand.main_image}`;
+    brandSection.appendChild(image);
+
+    //add description below image 
+    const description = document.createElement("p");
+    description.innerHTML = `${brand.description}`;
+    brandSection.appendChild(description);
+
+    //add array of available products 
+    const products = document.createElement("p");
+    products.className = "productsList";
+    products.innerHTML = `<b>Some Products offered by the brand:</b> ${brand.products.join(" , ")}`;
+
+
+
+}
