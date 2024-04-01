54cc310c05501601fb2a0af03891a4e754ceb674
Emma Hubert <ehubert@email.sc.edu>
2024-04-01 17:33:24 -0400

    semester project part 6 updated

--- Projects/part3/brands/script.js
+++ Projects/part3/brands/script.js
@@ -66,4 +66,143 @@ const getBrandSection = (brand) => {
 
 }
 
+
+const resetForm = () => {
+    const form = document.getElementById("add-brand-form");
+    form.reset();
+    document.getElementById("product-boxes").innerHTML = "";
+    document.getElementById("img-prev").src = "";
+};
+
+const showBrandForm = (e) => {
+    e.preventDefault();
+    openDialog("add-brand-form");
+    resetForm();
+};
+  
+const addProduct = (e) => {
+    e.preventDefault();
+    const section = document.getElementById("product-boxes");
+    const input = document.createElement("input");
+    input.classList.add("product-box");
+    input.type = "text";
+    input.placeholder = "enter product"
+    section.append(input);
+};
+  
+const openDialog = (id) => {
+    document.getElementById("dialog").style.display = "block";
+    document.querySelectorAll("#dialog-details > *").forEach((item) => {
+      item.classList.add("hidden");
+    });
+    document.getElementById(id).classList.remove("hidden");
+};
+
+const getProducts = () => {
+    const inputs = document.querySelectorAll("#product-boxes input");
+    const products = [];
+  
+    inputs.forEach((input)=>{
+        products.push(input.value);
+    });
+  
+    return products;
+}
+
+const cancel = () => {
+    resetForm();
+    document.getElementById("dialog").style.display = "none";
+}
+
 window.onload = () => showBrands();
+
+//document.getElementById("add-brand-form").onsubmit = addBrand;
+document.getElementById("brand-btn").onclick = showBrandForm;
+document.getElementById("add-product").onclick = addProduct;
+document.getElementById("cancel").onclick = cancel;
+  
+document.getElementById("img").onchange = (e) => {
+    if (!e.target.files.length) {
+      document.getElementById("img-prev").src = "";
+      return;
+    }
+    document.getElementById("img-prev").src = URL.createObjectURL(
+      e.target.files.item(0)
+    );
+};
+
+const showEmailResult = async(e) => {
+    e.preventDefault();
+  
+    const result = document.getElementById("result");
+    let response = await getEmailResult();
+  
+    if(response.status == 200){
+        result.innerHTML = "Successful!!";
+        submitBrandsForm(e);
+    } else {
+        result.innerHTML = "Sorry, your email was not sent";
+    }
+  
+  
+  };
+  
+  
+  const getEmailResult = async() => {
+    const form = document.getElementById("add-brand-form");
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+    const result = document.getElementById("result");
+    result.innerHTML = "Please wait....";
+  
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers:{
+                "Content-Type":"application/json",
+                Accept:"application/json"
+            },
+            body:json
+        });
+  
+        return response;
+    } catch(error){
+        console.log(error);
+        result.innerHTML = "Sorry, your submission couldn't be sent";
+    }
+  };
+  
+  
+  
+  const submitBrandsForm = (e) => {
+    
+    e.preventDefault();
+    const form = e.target;
+    const name = form.elements["name"].value;
+    //name.className = "name";
+    const description = form.elements["description"].value;
+    const image = form.elements["img"].value;
+    image.className = "images";
+
+    console.log(image);
+    const section = document.getElementById("brand-details");
+    const brandBox = document.createElement("section");
+    brandBox.className = "brand-box";
+    const favDiv = document.createElement("section");
+    const products = getProducts();
+    products.className = "productsList";
+    console.log(products);
+    favDiv.className = "brand";
+    favDiv.innerHTML = 
+    `<h2 class="name" >${name}</h2>
+    <img src=${image}>
+    <p>${description}</p>
+    <p><b>Some Products offered by the brand:</b> ${products.join(" , ")}</p>`
+    brandBox.appendChild(favDiv);
+    section.appendChild(brandBox);
+    form.reset();
+    cancel();
+  };
+
+document.getElementById("add-brand-form").onsubmit = showEmailResult;
